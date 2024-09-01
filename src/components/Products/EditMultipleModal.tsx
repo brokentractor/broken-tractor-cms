73faa4a011669a73f4f8374ac1cbfa6d09ad1e8c
Matthew Gilligan <tech@thedigitalhand.co>
2024-09-01 22:01:31 +0900

    Hacky batch update

--- src/components/Products/EditMultipleModal.tsx
+++ src/components/Products/EditMultipleModal.tsx
@@ -17,47 +17,50 @@ type EditMultipleModalProps = {
 
 const EditMultipleModal = ({ selectedProducts, optionSets, mutate }: EditMultipleModalProps) => {
   const [ removeLoading, setRemoveLoading ] = useState(false)
-  // const [ updateLoading, setUpdateLoading ] = useState(false)
+  const [ updateLoading, setUpdateLoading ] = useState(false)
   const [ optionSetID, setOptionSetID ] = useState<number | undefined>(undefined)
 
   const batchRemoveOptionSetID = async () => {
     setRemoveLoading(true)
   
     try {
-      // Convert the Set to an array and iterate over it
       const updatePromises = Array.from(selectedProducts).map((id) =>
         updateProduct(id.toString(), { option_set_id: null }),
       )
   
-      // Wait for all update operations to complete
       await Promise.all(updatePromises)
     }
     catch (error) {
       // eslint-disable-next-line no-console
-      console.error('Failed to update product:', error)
+      console.error('Failed to update products:', error)
     }
     finally {
       setRemoveLoading(false)
-      mutate() // Refresh or update the UI state
+      mutate()
     }
   }
 
-  // const updateOptionSetID = async () => {
-  //   setUpdateLoading(true)
-  //   if (optionSetID && optionSetID != product.option_set_id) {
-  //     try {
-  //       await updateProduct(product.id.toString(), { option_set_id: optionSetID })
-  //     }
-  //     catch (error) {
-  //     // eslint-disable-next-line no-console
-  //       console.error('Failed to update product:', error)
-  //     }
-  //     finally {
-  //       setUpdateLoading(false)
-  //       mutate()
-  //     }
-  //   }
-  // }
+  const batchUpdateOptionSetID = async () => {
+    setUpdateLoading(true)
+  
+    if (optionSetID) {
+      try {
+        const updatePromises = Array.from(selectedProducts).map((id) =>
+          updateProduct(id.toString(), { option_set_id: optionSetID }),
+        )
+    
+        await Promise.all(updatePromises)
+      }
+      catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Failed to update products:', error)
+      }
+      finally {
+        setUpdateLoading(false)
+        mutate()
+      }
+    }
+  }
 
   return (
     <Dialog>
@@ -77,7 +80,7 @@ const EditMultipleModal = ({ selectedProducts, optionSets, mutate }: EditMultipl
           </DialogDescription>
         </DialogHeader>
         
-        <form>
+        <form onSubmit={batchUpdateOptionSetID}>
           <div className="mb-6 grid gap-6">
             <div>
               <label htmlFor="optionSetSelect" className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
@@ -97,7 +100,7 @@ const EditMultipleModal = ({ selectedProducts, optionSets, mutate }: EditMultipl
             </div>
           </div>
           <div className='flex items-center justify-center gap-2'>
-            {/* <button 
+            <button 
               type="submit" 
               className="flex h-10 w-44 items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:dark:bg-gray-300 disabled:dark:hover:bg-gray-300"
               disabled={updateLoading 
@@ -106,7 +109,7 @@ const EditMultipleModal = ({ selectedProducts, optionSets, mutate }: EditMultipl
               }
             >
               {updateLoading ? <Spinner /> : 'Batch Update'}
-            </button> */}
+            </button>
             <button 
               className="flex h-10 w-44 items-center justify-center rounded-lg bg-red-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 disabled:dark:bg-gray-300 disabled:dark:hover:bg-gray-300"
               disabled={removeLoading}
