90260d919e1ed8b66eb16749914ce82ecd924f00
Matthew Gilligan <tech@thedigitalhand.co>
2024-09-01 16:38:37 +0900

    Imporve form functionality

--- src/components/Products/EditModal.tsx
+++ src/components/Products/EditModal.tsx
@@ -1,6 +1,7 @@
 
+import { useState } from 'react'
 import {
-  Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, 
+  Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, 
 } from '@/components/ui/dialog'
 import type { TProduct } from '@/interfaces/product'
 
@@ -9,6 +10,12 @@ type EditModalProps = {
 }
 
 const EditModal = ({ product }: EditModalProps) => {
+  const [ optionSetID, setOptionSetID ] = useState<string | undefined>(
+    product.option_set_id !== null ? product.option_set_id.toString() : undefined,
+  )
+  
+  console.log('optionSetID; ', optionSetID)
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -36,14 +43,16 @@ const EditModal = ({ product }: EditModalProps) => {
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                 placeholder="Enter Option Set ID"
                 required
-                value={product.option_set_id}
+                onChange={(e) => setOptionSetID(e.target.value)}
+                value={optionSetID}
               />
             </div>
           </div>
           <div className='flex items-center justify-center gap-2'>
             <button 
               type="submit" 
-              className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:hover:bg-gray-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:dark:bg-gray-300 disabled:dark:hover:bg-gray-300"
+              disabled={!optionSetID || optionSetID === product.option_set_id.toString() || optionSetID === '0'}
             >
               Update Option Set
             </button>
