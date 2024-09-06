e64065afc33c5fa770705601a93c881408fb8d74
Matthew Gilligan <tech@thedigitalhand.co>
2024-09-06 21:21:38 +0900

    Remove search form

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -68,18 +68,8 @@ const Products = ({
   
   return (
     <div className="relative my-20 w-full px-10">
-      <div className='fixed right-10 top-0 z-10 mb-2 flex w-[calc(100vw-336px)] justify-between gap-4 bg-white pt-10'>
-        <form className="mx-auto max-w-md">   
-          <label htmlFor="default-search" className="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">Search</label>
-          <div className="relative">
-            <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
-              <svg className="size-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-              </svg>
-            </div>
-            <input type="search" id="default-search" className="block w-full border border-gray-300 bg-gray-50 px-4 py-2.5 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" placeholder="Search Mockups, Logos..." required />
-          </div>
-        </form>
+      <div className='fixed right-10 top-0 z-10 mb-2 flex w-[calc(100vw-350px)] justify-between gap-4 bg-white pt-10'>
+
 
         <div className="flex-1">
           <select 
